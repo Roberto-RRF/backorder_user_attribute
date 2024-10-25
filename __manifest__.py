e2c8e8c4596c003509740bc6292ad8f9e6097c51
Roberto-RRF <69941000+Roberto-RRF@users.noreply.github.com>
2024-10-25 13:20:30 -0600

    upload

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Validate Parcial Stock Picking',
+    'version': '1.0',
+    'author':'ANFEPI: Roberto Requejo Fernández',
+    'depends': ['stock'],
+    'description': """
+    """,
+    'data': [
+        'security/backorder_user_attribute.xml',
+        'views/stock_backorder_confirmation_views.xml',
+    ],
+    'images': ['static/description/icon.png'],
+    'installable': True,
+    'auto_install': False,
+    "license": "AGPL-3",
+}
